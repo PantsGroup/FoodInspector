634b9d9efacc35832b41e62daf7a16ada77132ac
calvin_nguyen <imcalv@gmail.com>
2016-11-22 21:38:05 -0800

    initial commit

--- apps/food_inspectre/models.py
+++ apps/food_inspectre/models.py
@@ -1,5 +1,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import re
+from django.contrib import messages
+import bcrypt
+from datetime import datetime
+
+
+# class RestaurantManager(models.Manager):
 
 # Create your models here.
+class Restaurant(models.Model):
+	business_name = models.CharField(max_length=255)
+	address = models.CharField(max_length=255)
+	zip_code = models.CharField(max_length=20)
+	inspection_date = models.CharField(max_length=30)
+	inspection_score = models.IntegerField()
+	violation = models.TextField(max_length=1000)
+	risk_category = models.CharField(max_length=100)
+
+	# restMgr = RestaurantManager()
--- apps/food_inspectre/views.py
+++ apps/food_inspectre/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from .models import Restaurant
+from datetime import datetime
 
 def index(request):
     return render(request,"food_inspectre/index.html")
+
+def loadJSON(request):
+	if request.method == "POST":
+		print 'Raw Data: "%s"' % request.body
+	return redirect('/')
